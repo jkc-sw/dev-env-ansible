b984e194939ece51a39741e18f8e26b1502517fc
Kuanju Chen <guanru0919@yahoo.com>
2020-10-08 13:41:23 -0700

    better test and run on push again

--- rr.sh
+++ rr.sh
@@ -8,11 +8,29 @@ pushd &>/dev/null
 
 # container tag
 CONTAINER_TAG=devenvansible:1.0
+# test log file name
+TEST_LOG=./test.log
+# ansible workspace path
+ANSIBLE_WORKSPACE_PATH=/home/developer/dev-env-ansible
+
+# define a function to perform check
+check() {
+    # perform test
+    if ! grep -q 'changed=0' $TEST_LOG; then
+        echo "changed should be 0 at the secound run" >&2
+        exit 1
+    fi
+    if ! grep -q 'failed=0' $TEST_LOG; then
+        echo "failed should be 0 at the secound run" >&2
+        exit 2
+    fi
+}
 
 if [[ $# -lt 1 ]]; then
     # run ansible playbook
-    ansible-playbook playbook.yml -K
-    # ansible-playbook playbook.yml
+    ansible-playbook playbook.yml -K | tee $TEST_LOG
+    # ansible-playbook playbook.yml | tee $TEST_LOG
+    check
 else
     # if there are arg for test, run test
     case "$1" in
@@ -20,7 +38,7 @@ else
         # start bash inside container
         docker build --tag "$CONTAINER_TAG" . && \
         docker run --rm -it \
-            -v $SCRIPT_DIR:/home/developer/dev-env-ansible \
+            -v $SCRIPT_DIR:$ANSIBLE_WORKSPACE_PATH \
             "$CONTAINER_TAG" \
             bash
         ;;
@@ -28,16 +46,22 @@ else
         # build up the command here
         cmd='cd ./dev-env-ansible && ansible-playbook playbook.yml'
         cmd="$cmd && . ~/.bashrc && ansible-playbook playbook.yml"
-        cmd="$cmd | tee ./run1.log && grep changed=0 ./run1.log"
-        cmd="$cmd && grep failed=0 ./run1.log"
+        cmd="$cmd | tee $TEST_LOG"
         # this awesome blog post is worth the read
         # https://www.jeffgeerling.com/blog/2018/testing-your-ansible-roles-molecule
         # build and run the container
         docker build --tag "$CONTAINER_TAG" . && \
         docker run --rm \
-            -v $SCRIPT_DIR:/home/developer/dev-env-ansible \
+            -v $SCRIPT_DIR:$ANSIBLE_WORKSPACE_PATH \
             "$CONTAINER_TAG" \
             bash -c "$cmd"
+        ret=$?
+        if [[ $ret -ne 0 ]]; then
+            echo "Failed to provision the docker machine" >&2
+            exit $ret
+        fi
+        # perform check
+        check
         ;;
     *)
         # error out
