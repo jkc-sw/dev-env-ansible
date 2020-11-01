0c6a14d215f77101a1e126b77eb79cb1fd89c794
Kuanju Chen <guanru0919@yahoo.com>
2020-11-01 01:16:13 -0800

    Add multios support

--- rr.sh
+++ rr.sh
@@ -12,12 +12,18 @@ CONTAINER_TAG=devenvansible:1.0
 TEST_LOG=./test.log
 # ansible workspace path
 ANSIBLE_WORKSPACE_PATH=/home/developer/dev-env-ansible
+# dotfile path
+DOTFILE_PATH="$SCRIPT_DIR/../dotfiles"
 # docker files dir
 DOCKER_FILE_DIR=./dockerfiles
 # docker file name
 DOCKER_FILE_UBUNTU_20="$DOCKER_FILE_DIR/ubuntu2004"
 DOCKER_FILE_UBUNTU_18="$DOCKER_FILE_DIR/ubuntu1804"
 DOCKER_FILE_UBUNTU_16="$DOCKER_FILE_DIR/ubuntu1604"
+# docker repo name to be managed by rr.sh
+DEV_ENV_REPOSITORY_NAME=devenvansible
+RUN_PREFIX_FOR_NAME=run
+USE_PREFIX_FOR_NAME=use
 
 # define a function to perform check
 ansibleCheck() {
@@ -32,6 +38,7 @@ ansibleCheck() {
     fi
 }
 
+# use command to check if cmd is present
 checkCmd() {
     cmd=$1
     if ! command -v $cmd &>/dev/null; then
@@ -40,21 +47,48 @@ checkCmd() {
     fi
 }
 
+# When spawning a docker container, use a easily recognized name
+compose_container_name() {
+    prefix=$1
+    tag=$2
+    suffix=$RANDOM
+    echo -n "${prefix}_${tag}_${suffix}"
+}
+
 displayHelp() {
     # print help here
-    echo "${BASH_SOURCE[0]} [args...]"
-    echo "  Shortcuts for many things"
-    echo "args:"
-    echo " no arg          : Interactive install on host system asking for password"
-    echo " -h|--help|help  : Print this help command"
-    echo " install-i       : Install on the host system (mostly container) prompting for password"
-    echo " install         : Install on the host system (mostly container) w/o asking for password"
-    echo " check           : Do simple check on the host system for all executable"
-    echo " run [ver]       : Start the docker container only"
-    echo " run-build [ver] : Start the docker container and run ansible-playbook"
-    echo " run-test [ver]  : Start the docker container and run simple CI test"
-    echo "Where:"
-    echo " ver > Specify the version to use. Default 18. Currently supported '16, 18', '20'"
+    echo "${BASH_SOURCE[0]} [...]"
+    echo " One-stop shop for to do everything related to this repository"
+    echo ""
+    echo "Display Help"
+    echo "  -h|--help|h|hel|help  : Print this help command"
+    echo ""
+    echo "Running the ansible commands or related check"
+    echo "  install-i [-v]  : Install on the host system (when do it on your production machine) prompting for password"
+    echo "  install [-v]    : Install on the host system (mostly container) w/o asking for password"
+    echo "  check           : Do simple check on the host system for all executable"
+    echo ""
+    echo "Start a tmux session to develop this repo"
+    echo "  tmux            : Start the tmux development session"
+    echo ""
+    echo "This is used to manage the lifetime of the containers"
+    echo "  run [ver]       : Start a new docker container only"
+    echo "  run-build [ver] : Start a new docker container and run ansible-playbook"
+    echo "  commit ID TAG   : Commit a running docker container ID with the TAG specified"
+    echo "  use [TAG]       : Start a committed image only"
+    echo "  list            : List all the images"
+    echo "  use-build [TAG] : Start a committed image and run ansible-playbook"
+    echo ""
+    echo "This is used by CI to do testing"
+    echo "  run-test [ver]  : Start a new docker container and run simple CI test"
+    echo ""
+    echo "Options:"
+    echo "  -v > Provide the -vvv option to the ansigle command to have debug output"
+    echo ""
+    echo "Arguments:"
+    echo "  ver > Specify the version to use. Default 18. Currently supported '16, 18', '20'"
+    echo "  ID  > This is the container name or ID to use to make a commit, full name required"
+    echo "  TAG > This is by your preference on how the commited container to be tagged"
 }
 
 # see what version to use
@@ -63,16 +97,16 @@ select_docker_ver() {
     ver=$1
     # select the version
     case "$ver" in
-    *20*)
-        echo "$DOCKER_FILE_UBUNTU_20"
+    20)
+        echo -n "$DOCKER_FILE_UBUNTU_21"
         ;;
 
-    *18*)
-        echo "$DOCKER_FILE_UBUNTU_18"
+    18)
+        echo -n "$DOCKER_FILE_UBUNTU_18"
         ;;
 
-    *16*)
-        echo "$DOCKER_FILE_UBUNTU_16"
+    16)
+        echo -n "$DOCKER_FILE_UBUNTU_16"
         ;;
 
     *)
@@ -85,7 +119,7 @@ select_docker_ver() {
 
 # if there are arg for test, run test
 case "$1" in
-'-h'|'--help'|'help')
+'-h'|'--help'|'h'|'hel'|'help')
     displayHelp
     ;;
 
@@ -128,15 +162,37 @@ case "$1" in
 
 'install')
     # install with ansible playbook
-    ansible-playbook playbook.yml
+    if [[ $# -gt 1 ]]; then
+        ansible-playbook -vvv playbook.yml
+    else
+        ansible-playbook playbook.yml
+    fi
     ;;
 
 'install-i')
     # run ansible playbook
-    PY_COLORS=1 \
-    ANSIBLE_FORCE_COLOR=1 \
-    ansible-playbook playbook.yml -K | tee $TEST_LOG
+    if [[ $# -gt 1 ]]; then
+        PY_COLORS=1 \
+        ANSIBLE_FORCE_COLOR=1 \
+        ansible-playbook -vvv playbook.yml -K | tee $TEST_LOG
+    else
+        PY_COLORS=1 \
+        ANSIBLE_FORCE_COLOR=1 \
+        ansible-playbook playbook.yml -K | tee $TEST_LOG
+    fi
     ansibleCheck
+    ;;
+
+'tmux')
+    # check if we have a section already
+    if ! (tmux ls | grep -q ansible-env-dev); then
+        # start a new session
+        tmux new-session -s ansible-env-dev -n code -d -c "$SCRIPT_DIR"
+        tmux new-window -t ansible-env-dev:2 -n docker -c "$SCRIPT_DIR"
+        tmux new-window -t ansible-env-dev:3 -n dotfiles -c "$DOTFILE_PATH"
+    fi
+    # attach to the session
+    tmux attach-session -t ansible-env-dev:1
     ;;
 
 'run')
@@ -150,6 +206,7 @@ case "$1" in
     docker run --rm -it \
         --network="host" \
         -v $SCRIPT_DIR:$ANSIBLE_WORKSPACE_PATH \
+        --name "$(compose_container_name "$RUN_PREFIX_FOR_NAME" "$2")" \
         "$CONTAINER_TAG" \
         bash -i -c "cd ./dev-env-ansible ; bash -i"
     ;;
@@ -168,7 +225,52 @@ case "$1" in
     docker run --rm -it \
         --network="host" \
         -v $SCRIPT_DIR:$ANSIBLE_WORKSPACE_PATH \
+        --name "$(compose_container_name "$RUN_PREFIX_FOR_NAME" "$2")" \
         "$CONTAINER_TAG" \
+        bash -i -c "$cmd ; bash -i"
+    ;;
+
+'commit')
+    # get args
+    ident=$2
+    tag=$3
+    # make a commit
+    docker commit "$ident" "$DEV_ENV_REPOSITORY_NAME:$tag"
+    ;;
+
+'list')
+    # list all the images
+    echo "All the committed images"
+    docker images
+    echo ""
+    echo "All the running containers"
+    docker ps
+    ;;
+
+'use')
+    # get arg
+    tag=$2
+    # start bash inside container
+    docker run --rm -it \
+        --network="host" \
+        -v $SCRIPT_DIR:$ANSIBLE_WORKSPACE_PATH \
+        --name "$(compose_container_name "$RUN_PREFIX_FOR_NAME" "$tag")" \
+        "$DEV_ENV_REPOSITORY_NAME:$tag" \
+        bash -i -c "cd ./dev-env-ansible ; bash -i"
+    ;;
+
+'use-build')
+    # get arg
+    tag=$2
+    # build up the command here
+    cmd="cd ./dev-env-ansible && ./rr.sh install"
+    cmd="$cmd && . ~/.bashrc"
+    # start bash inside container
+    docker run --rm -it \
+        --network="host" \
+        -v $SCRIPT_DIR:$ANSIBLE_WORKSPACE_PATH \
+        --name "$(compose_container_name "$RUN_PREFIX_FOR_NAME" "$tag")" \
+        "$DEV_ENV_REPOSITORY_NAME:$tag" \
         bash -i -c "$cmd ; bash -i"
     ;;
 
@@ -199,5 +301,4 @@ case "$1" in
     displayHelp
     exit 1
     ;;
-
 esac
