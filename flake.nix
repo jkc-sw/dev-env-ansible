f8446b0bd780dbe147369aa5077e97a3551220ac
Kuanju Chen <kuanju0919@yahoo.com>
2025-05-31 04:25:54 +0000

    flake: only copy flake but not the rest of the git repo

--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           system = "${system}";
           overlays = [
@@ -34,46 +38,50 @@
         packages = with pkgs; [
           ansible
         ];
+      in {
+        formatter = pkgs.alejandra;
 
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-        devShells.default = pkgs.mkShell {
-          buildInputs = packages;
-          # The following var is used in ansible-playbook like this `-e "ansible_python_interpreter=$EXPLICIT_PYTHON_PATH_FOR_ANSIBLE"`
-          # I am using ansible_python_interpreter instead of ansible_playbook_python is because the remote host, aka localhost, needs
-          #   the pydebian package and I want ansible to use a secific one.
-          EXPLICIT_PYTHON_PATH_FOR_ANSIBLE = "${pythonEnvWithPyDebian}/bin/python3";
-          shellHook = ''
-            export IN_NIX_RR_SHELL=1
-            if [[ -z "$DISPLAY" ]]; then
-              export DISPLAY=:1
-            fi
-            if [[ -e /usr/lib/locale/locale-archive ]]; then
-                export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
-            else
-                echo 'WARN: Unable to export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive' >&2
-            fi
-            if [[ -e /etc/ssl/certs/ca-certificates.crt ]]; then
-                export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
-            else
-                echo 'WARN: Unable to export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt' >&2
-            fi
-          '';
-        };
+        devShells.default = let
+          fs = pkgs.lib.fileset;
+          sources = fs.unions [
+            ./flake.nix
+            ./flake.lock
+          ];
+        in
+          fs.trace sources (pkgs.mkShell {
+            buildInputs = packages;
+            src = fs.toSource {
+              root = ./.;
+              fileset = sources;
+            };
+            EXPLICIT_PYTHON_PATH_FOR_ANSIBLE = "${pythonEnvWithPyDebian}/bin/python3";
+            shellHook = ''
+              export IN_NIX_RR_SHELL=1
+              if [[ -z "$DISPLAY" ]]; then
+                export DISPLAY=:1
+              fi
+              if [[ -e /usr/lib/locale/locale-archive ]]; then
+                  export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+              else
+                  echo 'WARN: Unable to export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive' >&2
+              fi
+              if [[ -e /etc/ssl/certs/ca-certificates.crt ]]; then
+                  export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+              else
+                  echo 'WARN: Unable to export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt' >&2
+              fi
+            '';
+          });
       }
     );
 }
-
 /*
-  SOURCE_THESE_VIMS_START
-  nnoremap <leader>no <cmd> silent call execute(escape("!tmux send-keys -t :.+1 './shell.sh' Enter", '#'))<cr>
-  nnoremap <leader>ne <cmd> silent call execute(escape("!tmux send-keys -t :.+1 'export NIXPKGS_ALLOW_UNFREE=1 ; nix develop --impure . -L' Enter", '#'))<cr>
-  nnoremap <leader>nu <cmd> silent call execute(escape("!tmux send-keys -t :.+1 'git push' Enter", '#'))<cr>
-  echom 'Sourced'
-  SOURCE_THESE_VIMS_END
+SOURCE_THESE_VIMS_START
+nnoremap <leader>no <cmd> silent call execute(escape("!tmux send-keys -t :.+1 './shell.sh' Enter", '#'))<cr>
+nnoremap <leader>ne <cmd> silent call execute(escape("!tmux send-keys -t :.+1 'export NIXPKGS_ALLOW_UNFREE=1 ; nix develop --impure . -L' Enter", '#'))<cr>
+nnoremap <leader>nu <cmd> silent call execute(escape("!tmux send-keys -t :.+1 'git push' Enter", '#'))<cr>
+echom 'Sourced'
+SOURCE_THESE_VIMS_END
 */
-
 # vim:et ts=2 sts=2 sw=2
-
 
