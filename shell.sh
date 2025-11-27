36ce95baf420cc2a45079605fda0eb0d4eb5b379
Kuanju Chen <kuanju0919@yahoo.com>
2025-11-27 21:25:55 +0000

    shell: use lix installer

--- shell.sh
+++ shell.sh
@@ -17,14 +17,14 @@ if ! command -v nix &>/dev/null; then
     echo 'WARN: nix is not found in your PATH' >&2
     echo 'WARN: --------------------------------------------------------------------------' >&2
     echo 'WARN: To install nix, the following commands would be run.' >&2
-    echo "WARN: \$ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install" >&2
+    echo "WARN: \$ curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install" >&2
     echo 'WARN: --------------------------------------------------------------------------' >&2
     totalCount=3
     for count in $(seq $totalCount); do
         echo "WARN: Asking $count/$totalCount. Select 'y' to automatically run the scripts above, or 'n' to abort." >&2
         read -r -p "> " res
         if [[ "$res" == 'y' || "$res" == 'Y' ]]; then
-            if curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install; then
+            if curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install; then
                 echo 'INFO: Installation completed.' >&2
                 if [[ ! -r /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
                     echo 'ERR: Nix installation failed or nix not found' >&2
